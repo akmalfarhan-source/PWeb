98fdb018eea6e1383a6023a15f85a5a5157020d6
macbook <macbook@MacBook-Pro-M1.local>
2026-05-07 21:02:22 +0700

    add index.php & pertemuan 7

---
+++ views/pertemuan7.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php include '../Asets/header.php' ?>
+</head>
+
+<body class="layout-fixed sidebar-expand-lg bg-body-tertiary">
+    <!--begin::App Wrapper-->
+    <div class="app-wrapper">
+        <!--begin::Header-->
+        <?php include '../Asets/Navbar.php' ?>
+        <!--end::Header-->
+        <!--begin::Sidebar-->
+        <?php include '../Asets/Sidebar.php' ?>
+    </div>
+    <!--end::App Wrapper-->
+    <!--begin::Script-->
+    <!--begin::Third Party Plugin(OverlayScrollbars)-->
+    <?php include '..Asets/script.php' ?>
+</body>
+
+</html>
